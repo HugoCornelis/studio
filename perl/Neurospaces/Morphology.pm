102aa9c762cd41be3266109d87bb6125639fa4b2
Hugo Cornelis <hugo.cornelis@gmail.com>
2007-10-10 19:37:38 +0000

    1. Added subs ->cumulated_length(), ->membrane_area() and ->volume()    to Morphology.pm.

--- perl/Neurospaces/Morphology.pm
+++ perl/Neurospaces/Morphology.pm
@@ -18,6 +18,21 @@ our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(all_morphologies);
 
 
+sub cumulated_length
+{
+    my $self = shift;
+
+    my $component_name = shift;
+
+    if (!$self->{length_surface_volume})
+    {
+	$self->length_surface_volume($component_name);
+    }
+
+    return $self->{length_surface_volume}->{total_length};
+}
+
+
 sub dendritic_tips
 {
     my $self = shift;
@@ -133,6 +148,99 @@ sub instantiate_backend
 }
 
 
+sub membrane_area
+{
+    my $self = shift;
+
+    my $component_name = shift;
+
+    if (!$self->{length_surface_volume})
+    {
+	$self->length_surface_volume($component_name);
+    }
+
+    return $self->{length_surface_volume}->{total_surface};
+}
+
+
+sub length_surface_volume
+{
+    my $self = shift;
+
+    my $component_name = shift;
+
+    my $in_memory = 0;
+
+    if ($in_memory)
+    {
+# 	# get context
+
+# 	my $context = SwiggableNeurospaces::PidinStackParse($component_name);
+
+# 	# get component
+
+# 	my $component = $context->PidinStackLookupTopSymbol();
+
+# 	$component->SymbolLinearize($context);
+
+    }
+
+    else
+    {
+	my $self_commands
+	    = join
+		' ',
+		    (
+		     map
+		     {
+			 "--command '$_'"
+		     }
+		     @{$self->{commands}},
+		    );
+
+	my $self_options
+	    = join
+		' ',
+		    (
+		     map
+		     {
+			 "--backend-option '$_'"
+		     }
+		     @{$self->{backend_options}},
+		    );
+
+	my $system_command = "neurospaces $self_options $self_commands --command 'printparameter $component_name TOTALLENGTH' --command 'printparameter $component_name TOTALSURFACE' --command 'printparameter $component_name TOTALVOLUME' \"$self->{filename}\"";
+
+	print STDERR "executing ($system_command)\n";
+
+	my $output_command = join '', `$system_command`;
+
+	$output_command =~ /TOTALLENGTH.*?= (\S+)/s;
+
+	my $total_length = $1;
+
+	$output_command =~ /TOTALSURFACE.*?= (\S+)/s;
+
+	my $total_surface = $1;
+
+	$output_command =~ /TOTALVOLUME.*?= (\S+)/s;
+
+	my $total_volume = $1;
+
+	$self->{length_surface_volume}->{total_length} = $total_length;
+
+	$self->{length_surface_volume}->{total_surface} = $total_surface;
+
+	$self->{length_surface_volume}->{total_volume} = $total_volume;
+
+    }
+
+    # return result
+
+    return $self->{length_surface_volume};
+}
+
+
 sub load
 {
     my $self = shift;
@@ -177,6 +285,111 @@ sub new
 #     }
 
     return $self;
+}
+
+
+sub somatopetaldistances
+{
+    my $self = shift;
+
+    my $component_names = shift;
+
+    my $result;
+
+    my $in_memory = 0;
+
+    if ($in_memory)
+    {
+# 	# get context
+
+# 	my $context = SwiggableNeurospaces::PidinStackParse($component_name);
+
+# 	# get component
+
+# 	my $component = $context->PidinStackLookupTopSymbol();
+
+# 	$component->SymbolLinearize($context);
+
+    }
+
+    else
+    {
+	my $self_commands
+	    = join
+		' ',
+		    (
+		     map
+		     {
+			 "--command '$_'"
+		     }
+		     @{$self->{commands}},
+		    );
+
+	my $self_options
+	    = join
+		' ',
+		    (
+		     map
+		     {
+			 "--backend-option '$_'"
+		     }
+		     @{$self->{backend_options}},
+		    );
+
+	my $self_component_names
+	    = join
+		' ',
+		    (
+		     map
+		     {
+			 "--command 'printparameter $_ SOMATOPETAL_DISTANCE'"
+		     }
+		     @$component_names,
+		    );
+
+
+	my $system_command1 = "neurospaces $self_options $self_commands $self_component_names \"$self->{filename}\"";
+
+	print STDERR "executing ($system_command1)\n";
+
+	my $yaml_distances_string = join '', `$system_command1`;
+
+	$yaml_distances_string =~ s/.*---/---/gs;
+
+# 	$yaml_distances_string =~ s/\n.*$/\n/;
+
+# 	print "($yaml_distances_string)";
+
+	use YAML;
+
+	my $distances = Load($yaml_distances_string);
+
+	$result->{somatopetaldistances} = $distances;
+
+    }
+
+    # cache result
+
+    $self->{somatopetaldistances} = $result;
+
+    # return result
+
+    return $result;
+}
+
+
+sub volume
+{
+    my $self = shift;
+
+    my $component_name = shift;
+
+    if (!$self->{length_surface_volume})
+    {
+	$self->length_surface_volume($component_name);
+    }
+
+    return $self->{length_surface_volume}->{total_volume};
 }
 
 
